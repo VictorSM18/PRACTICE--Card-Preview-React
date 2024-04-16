f13a1fa5f67563f3548e75afa80eec306fe4287c
Victor Samuel <victoress18@gmail.com>
2024-04-15 20:08:09 -0400

    Fixing more issues

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
 export const data = {
-  portrait: './src/assets/images/illustration-article.svg',
+  portrait: '/illustration-article.svg',
   tag: 'Learning',
   date: 'Published 21 Dec 2023',
   title: 'HTML & CSS Foundations',
   description:
     'These languages are the backbone of every website, defining structure, content, and presentation.',
   authorName: 'Greg Hooper',
-  authorPicture: './src/assets/images/image-avatar.webp',
+  authorPicture: '/image-avatar.webp',
 }
