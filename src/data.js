3f98ec4edfd2500b6725c83146391cf4fb4f7697
Victor Samuel <victoress18@gmail.com>
2024-04-15 23:28:02 -0400

    fixed production

--- src/data.js
+++ src/data.js
@@ -1,10 +1,13 @@
+import imgPortrait from './assets/images/illustration-article.svg'
+import imgPicture from './assets/images/image-avatar.webp'
+
 export const data = {
-  portrait: '/illustration-article.svg',
+  portrait: imgPortrait,
   tag: 'Learning',
   date: 'Published 21 Dec 2023',
   title: 'HTML & CSS Foundations',
   description:
     'These languages are the backbone of every website, defining structure, content, and presentation.',
   authorName: 'Greg Hooper',
-  authorPicture: '/image-avatar.webp',
+  authorPicture: imgPicture,
 }
