6d5c37f006c77eab8c2b847b80b96135c653506b
Victor Samuel <victoress18@gmail.com>
2024-04-15 20:40:39 -0400

    Fixing blank page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/PRACTICE--Card-Preview-React/',
   plugins: [react()],
 })
